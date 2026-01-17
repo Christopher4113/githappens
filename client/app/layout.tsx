9ccf6bc2a0e479124a4c8a53a82e8c929c6b2c2f
Luca Porto Duarte <lduarte@uwo.ca>
2026-01-17 13:22:11 -0500

    quick fix

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -43,10 +43,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Amplitude />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Amplitude />
         {children}
       </body>
     </html>
